6691ee1c66ba440c049697be38b909f6095c86e9
yuuki3353 <syuukatu3353@gmail.com>
2022-07-20 16:51:00 +0900

    デプロイ

--- database/migrations/2022_07_20_163435_create_schedules_table.php
+++ database/migrations/2022_07_20_163435_create_schedules_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-    return new class extends Migration
-//class CreateSchedulesTable extends Migration
+class CreateSchedulesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -32,4 +31,4 @@ use Illuminate\Support\Facades\Schema;
     {
         Schema::dropIfExists('schedules');
     }
-};
+}
